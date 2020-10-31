a938cb92f588eb67f233c316f081f12dd154b5a8
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2020-10-31 21:20:15 +0200

    Update Get_name&price.py

--- Get_name&price.py
+++ Get_name&price.py
@@ -14,6 +14,11 @@ page_soup = soup(wep_page, "html.parser")
 # grabs all product
 containers_ads = page_soup.find_all("article", {"class":"prd _fb col c-prd"})
 
+filename = "products.csv"
+f = open(filename, "w")
+headser = "product name, Price\n"
+f.write(headser)
+
 for container in containers_ads:
     # title ads
     title_container = container.findAll("h3",{"class": "name"})
@@ -21,7 +26,9 @@ for container in containers_ads:
     # price ads
     price_container = container.findAll("div", {"class": "prc"})
     price = price_container[0].text.strip()
-
-
+    
     print("product name : " + product_name)
     print("price ads : " + price)
+    f.write(product_name + "," + price + "\n")
+    
+f.close()
