614e3ba7c0c5ddb9f9dea12be9ffea6173598c27
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2020-10-31 21:13:04 +0200

    Create Get_name&price.py

---
+++ Get_name&price.py
@@ -0,0 +1,27 @@
+
+from urllib.request import Request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+
+
+my_url = "https://www.jumia.com.eg/smartphones/"
+req = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req)
+wep_page = webpage.read()
+webpage.close()
+
+page_soup = soup(wep_page, "html.parser")
+# grabs all product
+containers_ads = page_soup.find_all("article", {"class":"prd _fb col c-prd"})
+
+for container in containers_ads:
+    # title ads
+    title_container = container.findAll("h3",{"class": "name"})
+    product_name = title_container[0].text.strip()
+    # price ads
+    price_container = container.findAll("div", {"class": "prc"})
+    price = price_container[0].text.strip()
+
+
+    print("product name : " + product_name)
+    print("price ads : " + price)
